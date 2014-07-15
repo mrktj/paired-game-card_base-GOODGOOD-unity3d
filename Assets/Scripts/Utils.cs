2fa9ec07244f7c504d07b19724f89a629029c906
Charles Amat <charlie.amat@force5solutions.com>
2014-07-15 08:03:50 -0400

    Created static AddChild function to Utils - This is helper method for placing children into NGUI's UIGrid

--- Assets/Scripts/Utils.cs
+++ Assets/Scripts/Utils.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using UnityEngine;
+using Random = System.Random;
 
 namespace Assets.Scripts
 {
@@ -15,6 +16,19 @@ namespace Assets.Scripts
                 T tmp = array[j];
                 array[j] = array[i - 1];
                 array[i - 1] = tmp;
+            }
+        }
+
+        public static void AddChild(GameObject parent, GameObject child)
+        {
+            if (child != null && parent != null)
+            {
+                var t = child.transform;
+                t.parent = parent.transform;
+                t.localPosition = Vector3.zero;
+                t.localRotation = Quaternion.identity;
+                t.localScale = Vector3.one;
+                child.layer = parent.layer;
             }
         }
     }
